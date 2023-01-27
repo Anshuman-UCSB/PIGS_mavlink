983a2b692c13936c5c86443d436de12c4af22202
Anshuman Dash <anshuman@ucsb.edu>
2023-01-27 12:41:16 -0800

    Added arm first

--- drone.py
+++ drone.py
@@ -3,6 +3,12 @@ class Drone:
 	def __init__(self):
 		self.conn = mavutil.mavlink_connection('udpin:localhost:14551')
 		self.conn.wait_heartbeat()
+	def arm(self):
+		self.conn.mav.command_long_send(self.conn.target_system, self.conn.target_component,
+                                     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
+
+		msg = self.conn.recv_match(type='COMMAND_ACK', blocking=True)
+		print(msg)
 	def guided(self):
 		self.conn.mav.command_long_send(self.conn.target_system, self.conn.target_component,
                                      mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
@@ -32,5 +38,6 @@ class Drone:
 
 if __name__ == "__main__":
 	d = Drone()
+	d.arm()
 	d.guided()
 	d.takeoff()
